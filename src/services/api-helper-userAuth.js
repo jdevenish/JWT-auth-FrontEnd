a8ca52a90a690a9a3412fd76d29e9204f810c044
jdevenish <justin.devenish@gmail.com>
2020-04-18 20:07:05 -0400

    Adjusting headers

--- src/services/api-helper-userAuth.js
+++ src/services/api-helper-userAuth.js
@@ -18,10 +18,7 @@ export const registerNewUser = async (user) => {
 
 export const authenticateUser = async (user) => {
     const resp = await api.post('/authenticate', user, {
-        withCredentials: true,
-        headers: {
-            "Access-Control-Allow-Origin": "https://heuristic-carson-cc6c87.netlify.app/"
-        }
+        withCredentials: true
         });
     console.log("authenticate user response: ", resp)
     return resp.data
